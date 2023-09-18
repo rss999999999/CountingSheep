b34ded8321705c746355b8e1a35e9ce6e06cc3e9
Rodrigo <rss.999999999@gmail.com>
2023-09-18 08:55:41 -0300

    Counting Sheeeps (array/boolean)

--- src/Counter.java
+++ src/Counter.java
@@ -1,5 +1,15 @@
-Cpublic class Counter {
+
+class Counter {
     public int countSheeps(Boolean[] arrayOfSheeps) {
-        // TODO May the force be with you
+        int count = 0;
+
+        //This line iterates through the arrayOfSheeps array and assigns the current element to the sheep variable, that can be true or false.
+        for (Boolean sheep : arrayOfSheeps) {
+            //This line checks if the current element sheep is not null and is true. If both conditions are true, the code inside the if statement will be executed.
+            if (sheep != null && sheep) {
+                count++;
+            }
+        }
+        return count;
     }
 }
